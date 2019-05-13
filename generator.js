ff6380124c1f34bf7e06f057527a09f7e39c4bf0
Vivien <vdebona@umanit.fr>
2019-05-13 13:24:09 +0200

    Change parameter handling and accept HTML code
    - Use JSON body to get parameters
    - Check that URL or HTML are passed and type is valid
    - Make generator handle HTML code

--- generator.js
+++ generator.js
@@ -1,22 +1,24 @@
 const generator = require('puppeteer');
 
-module.exports = (url, type) => (async () => {
-  if ('png' !== type && 'pdf' !== type) {
-    throw new Error('Invalid type "' + type + '", must be "png" or "pdf".');
-  }
-
+module.exports = (urlOrHtml, type) => (async () => {
   const browser = await generator.launch({
     headless: true,
     executablePath: '/usr/bin/chromium-browser',
     args: ['--disable-dev-shm-usage'],
   });
   const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'networkidle0'});
 
-  const checkboxSelector = 'label[for="confirmLegals"]';
-  await page.waitFor(checkboxSelector);
-  await page.click(checkboxSelector);
-  await page.waitFor(300);
+  // "http" is not the four firth characters, so we guess it's HTML code
+  if ('http' !== urlOrHtml.charAt(0) + urlOrHtml.charAt(1) + urlOrHtml.charAt(2) + urlOrHtml.charAt(3)) {
+    await page.setContent(urlOrHtml, {waitUntil: 'networkidle0'});
+  } else {
+    await page.goto(urlOrHtml, {waitUntil: 'networkidle0'});
+  }
+
+  // const checkboxSelector = 'label[for="confirmLegals"]';
+  // await page.waitFor(checkboxSelector);
+  // await page.click(checkboxSelector);
+  // await page.waitFor(300);
 
   let buffer;
 
--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const http = require('http');
-const url = require('url');
 const generator = require('./generator');
 
 const server = http.createServer(async (req, res) => {
-  const queryData = url.parse(req.url, true).query;
-
-  if (!queryData.url) {
+  if ('post' !== req.method.toLowerCase()) {
     res.writeHead(500);
-    res.end('URL is missing.');
+    res.end('Bad method');
 
     return;
   }
 
-  const type = queryData.type || 'png';
+  let body = '';
 
-  try {
-    const buffer = await generator(queryData.url, type);
-    const contentType = 'png' === type ? 'image/png' : 'application/pdf';
+  req.on('data', chunk => {
+    body += chunk;
+  });
 
-    res.writeHead(200, {'Content-Type': contentType});
-    res.end(buffer);
-  } catch (e) {
-    res.writeHead(500);
-    res.end(e.message);
-  }
+  req.on('end', async () => {
+    try {
+      const {url, html, type, ...params} = JSON.parse(body);
+
+      if (!type) {
+        throw new Error('The parameter "type" is required.');
+      }
+
+      if ('png' !== type && 'pdf' !== type) {
+        throw new Error('Invalid type "' + type + '", must be "png" or "pdf".');
+      }
+
+      if ((!url && !html) || (url && html)) {
+        throw new Error('Either "url" or "html" must be passed, not both.');
+      }
+
+      const buffer = await generator(url || html, type);
+      const contentType = 'png' === type ? 'image/png' : 'application/pdf';
+
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(buffer);
+    } catch (e) {
+      res.writeHead(500);
+      res.end(e.message);
+    }
+  });
 });
 
 server.listen(3000);
