3146fd83aa2fe5722098c49782d4aaada95048b8
DjLeChuck <djlechuck@gmail.com>
2019-07-25 22:45:26 +0200

    Load custom middlewares

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import '@babel/polyfill';
 
 import http from 'http';
 import connect from 'connect';
+import path from 'path';
+import fs from 'fs';
 import checkMethod from './middlewares/check-method';
 import generate from './middlewares/generate';
 import errorHandling from './middlewares/error-handling';
@@ -14,6 +16,24 @@ const PORT = process.env.PORT || 1337;
 
 const app = connect();
 
+// Load custom middlewares
+try {
+  const directoryPath = path.join(__dirname, 'middlewares', 'custom');
+
+  const files = fs.readdirSync(directoryPath);
+
+  files.forEach(file => {
+    if ('.js' !== path.extname(file)) {
+      return;
+    }
+
+    app.use(require(path.join(directoryPath, file)));
+  });
+} catch (err) {
+  throw new Error('Error while adding custom middlewares: ' + err);
+}
+
+// Load service middlewares
 app.use(checkMethod);
 app.use(generate);
 app.use(errorHandling);
